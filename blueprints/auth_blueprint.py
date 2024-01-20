87cb5c8e7c2e07c280ba6872c7e85e0601c62cca
dkbaldwin <87735707+dkbaldwin@users.noreply.github.com>
2024-01-20 03:01:10 -0600

    #56 Applications needs to require username and password credentials to login (#57)
    * this code adds a password column to the user table
    * it also updates the auth_blueprint to include password validation.
    * and adds type annotation to key variables and function names where applicable.

--- blueprints/auth_blueprint.py
+++ blueprints/auth_blueprint.py
@@ -2,9 +2,45 @@ from flask import render_template, url_for, redirect
 from flask_login import LoginManager, login_required, current_user, logout_user, login_user
 from flask import Flask, render_template, jsonify, request, flash, url_for, redirect
 from pathlib import Path
+from passlib.exc import MalformedHashError, InvalidHashError
+from passlib.hash import pbkdf2_sha256
+from sqlalchemy.exc import NoResultFound, MultipleResultsFound, SQLAlchemyError
 
 from . import auth_bp
 from models import User
+
+
+def load_user(username: str):
+    try:
+        user: User = User.query.filter_by(username=username).first()
+    except NoResultFound:
+        user = None
+    except MultipleResultsFound:
+        # Handle the case where multiple users with the same username are found
+        # This should not be possible but still want to cover for it because the exception is possible.
+        raise Exception("Multiple users found with the same username")
+    except SQLAlchemyError as e:
+        # Handle other SQLAlchemy errors
+        print(f"error occurred: {e}")
+        user = None
+
+    return user
+
+
+def hash_password(password):
+    # Hash the password using PBKDF2
+    return pbkdf2_sha256.hash(password)
+
+
+def verify_password(username, password, hashed_password):
+    match_found = False
+    try:
+        match_found = pbkdf2_sha256.verify(password, hashed_password)
+    except (MalformedHashError, InvalidHashError) as e:
+        print(f"Malformed / Invalid Hash Error when verifying password for {username}")
+    except ValueError as e:
+        print(f"Unexpected error when validating password for {username}")
+    return match_found
 
 
 # renders the manual login page
@@ -22,14 +58,21 @@ def landing_page():
 
 @auth_bp.route('/login', methods=['POST'])
 def login_validate():
-    username = request.form['username']
-    user = User.query.filter_by(username=username).first()
-    # TODO: and check_password_hash(user.password_hash, password)
-    if user:
-        # If authentication is successful, log the user in
-        login_user(user)
-        flash('Logged in successfully!', 'success')
+
+    if current_user.is_authenticated:
         return redirect(url_for('auth.landing_page'))
+
+    form_username: str = request.form['username']
+    form_password: str = request.form['password']
+
+    user: User = load_user(form_username)
+    password: str = form_password
+
+    if user:
+        if verify_password(user.username, password, user.password):
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('auth.landing_page'))
 
     flash('Login failed. Please check your credentials and try again.', 'danger')
     return redirect(url_for('auth.login'))
