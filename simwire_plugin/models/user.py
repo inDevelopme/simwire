1f5fe6b2e44de25b9bdc02d82dbcff6b2e5796ba
dkbaldwin <dkbaldwin@indevelopme.com>
2024-01-22 06:45:58 -0600

    #62 application should include db migrations features
    * testing how the flask-migration works
    
    `flask --app=migration-application db init`
    `flask  --app=migration-application db migrate -m "Initial migration."`
    `flask  --app=migration-application db migrate -m "test adding a nickname column to the user table"`
    `flask  --app=migration-application db upgrade`

--- simwire_plugin/models/user.py
+++ simwire_plugin/models/user.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    nickname = db.Column(db.String(45), nullable=True)
 
     def __init__(self, username):
         self.username = username
