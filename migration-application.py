bd331ee70175584ff454d876aff86af2cfba938c
dkbaldwin <dkbaldwin@indevelopme.com>
2024-01-25 07:23:41 -0600

    #70 Our application should modularize blueprints components
    Removed references for blueprint and cors because migration is only responsible for database updates and those database updates are contained in the models directory.

--- migration-application.py
+++ migration-application.py
@@ -3,12 +3,9 @@
 from flask import Flask
 # The removal of the relative path is the solution to removing the codebase
 from .simwire_plugin.env_load import Config
-from flask_cors import CORS
-from .simwire_plugin.blueprints import home_bp, auth_bp, admin_bp
 from .simwire_plugin.models import db, migrate
 
 app = Flask(__name__)
-CORS(app)
 
 config = Config()
 config.get_environment_config()
@@ -21,13 +18,3 @@ app.config.from_object(config)
 db.init_app(app)
 migrate.init_app(app, db)
 
-app.register_blueprint(home_bp)
-app.register_blueprint(auth_bp)
-app.register_blueprint(admin_bp)
-
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    app.debug = True
-    app.run()
