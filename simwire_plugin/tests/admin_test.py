853bdb488e3dc815525f3d145ad0a9d91b1b6e0f
dkbaldwin <dkbaldwin@indevelopme.com>
2024-01-28 23:48:33 -0600

    #75 Everything required to run the application inside a container should
    implementing relative path for test cases

--- simwire_plugin/tests/admin_test.py
+++ simwire_plugin/tests/admin_test.py
@@ -1,16 +1,17 @@
-# tests/test_routes.py
 import pytest
-from flask import Flask
-from simwire_plugin import app
+from .. import app
+
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_health_check_route(client):
     response = client.get('/health_check')
     assert response.status_code == 200
+
 
 def test_example_route(client):
     response = client.get('/')  # Update with your actual route
